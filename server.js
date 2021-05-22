2ed9bbd63758dd81c2e50dc14f32b9357dc60e57
Luiz Gustavo de Freitas <38074380+luizgustavo77@users.noreply.github.com>
2021-05-22 18:10:38 -0300

    add swagger

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+let swaggerDocument = require('./swagger_output.json');
+const express = require('express');
+const api = express();
+
 require('rootpath')();
 let config = require("./config.json");
 let cors = require('cors');
 let expressJwt = require('express-jwt');
 
-let api = express();
+/*Routes*/
+const router = require('express').Router();
+
 api.use(cors());
 api.use(express.urlencoded());
 api.use(express.json());
@@ -16,8 +21,12 @@ api.use('/api/perfil', require('./controllers/perfil.controller'));
 api.use('/api/formulario', require('./controllers/formulario.controller'));
 api.use('/api/solicitacao', require('./controllers/solicitacao.controller'));
 
+/* Middlewares */
+api.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 var apiPort = process.env.PORT || config.port;
 
 var serverAPI = api.listen(apiPort, function () {
     console.log('Server API listening at http://localhost:' + serverAPI.address().port);
+    console.log("Server is running!\nAPI documentation: http://localhost:3000/api-doc");
 });
